ca36f9d09392f69558eac3cb12d3865bfa09146c
Félix Girault <felix.girault@gmail.com>
2022-05-14 00:51:20 +0200

    Fixed broken packing tests

--- src/lib/packing/v1.test.ts
+++ src/lib/packing/v1.test.ts
@@ -2,7 +2,6 @@ import {schemistColor} from '$lib/color/spaces';
 import defs from '$lib/definitions/nodes';
 import color from '$lib/definitions/nodes/color';
 import presets from '$lib/definitions/presets';
-import schemistPreset from '$lib/definitions/presets/schemist';
 import {defaultArgs} from '$lib/stores/nodes';
 import {pack, unpack} from './v1';
 
@@ -19,18 +18,21 @@ test.each(Object.values(defs))('pack node $label', ({type}) => {
 	expect(unpacked).toMatchObject(tree);
 });
 
-test.each(Object.values(presets))('pack preset $label', () => {
-	const tree = {
-		type: color.type,
-		args: {
-			color: schemistColor('#f00')
-		},
-		children: schemistPreset.nodes
-	};
+test.each(Object.values(presets))(
+	'pack preset $label',
+	({nodes}) => {
+		const tree = {
+			type: color.type,
+			args: {
+				color: schemistColor('#f00')
+			},
+			children: nodes
+		};
 
-	const packed = pack(tree);
-	const unpacked = unpack(packed);
+		const packed = pack(tree);
+		const unpacked = unpack(packed);
 
-	expect(packed).toMatchSnapshot();
-	expect(unpacked).toMatchObject(tree);
-});
+		expect(packed).toMatchSnapshot();
+		expect(unpacked).toMatchObject(tree);
+	}
+);
