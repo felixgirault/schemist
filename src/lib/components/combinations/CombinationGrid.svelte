0182f8257e1b821557eceba42e5ccd2083f127c8
Félix Girault <felix.girault@gmail.com>
2022-08-10 00:32:24 +0200

    WIP

--- src/lib/components/combinations/CombinationGrid.svelte
+++ src/lib/components/combinations/CombinationGrid.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
-	import {hexColor} from '$lib/color/spaces';
+	import {
+		wcag2Contrast,
+		wcag2Grade,
+		wcag3Contrast,
+		wcag3Grade
+	} from '$lib/color/contrast';
+	import {cssColor, hexColor} from '$lib/color/spaces';
 	import Combination from '$lib/components/combinations/Combination.svelte';
 	import ContrastTypeFilter from '$lib/components/combinations/ContrastTypeFilter.svelte';
 	import LuminosityFilter from '$lib/components/combinations/LuminosityFilter.svelte';
 	import {
 		colorEntries,
 		combinations,
+		contrastType,
+		minLevel,
 		previewedCombinationUid,
 		selectedCombinations
 	} from '$lib/stores/combinations';
+	import {round} from '$lib/utils/math';
 	import Sample from '../Sample.svelte';
+	import ContrastFilter from './ContrastFilter.svelte';
 
 	const handlePreview = (uid: string) => {
 		$previewedCombinationUid =
@@ -17,6 +27,42 @@
 	};
 
 	$: colorCount = $colorEntries?.length || 0;
+
+	$: levelFn =
+		$contrastType === 'wcag2' ? wcag2Contrast : wcag3Contrast;
+	$: gradeFn =
+		$contrastType === 'wcag2' ? wcag2Grade : wcag3Grade;
+
+	const selectBackgrounds = (bgId) => {
+		const areAllSelected = $colorEntries.every(
+			({id}) =>
+				id === bgId || $selectedCombinations?.[bgId]?.[id]
+		);
+
+		$selectedCombinations[bgId] = Object.fromEntries(
+			$colorEntries.map(({id}) => [
+				id,
+				id === bgId ? false : !areAllSelected
+			])
+		);
+	};
+
+	const selectForegrounds = (fgId) => {
+		const areAllSelected = $colorEntries.every(
+			({id}) =>
+				id === fgId || $selectedCombinations?.[id]?.[fgId]
+		);
+
+		$selectedCombinations = Object.fromEntries(
+			$colorEntries.map(({id}) => [
+				id,
+				{
+					...($selectedCombinations?.[id] || {}),
+					[fgId]: id === fgId ? false : !areAllSelected
+				}
+			])
+		);
+	};
 </script>
 
 <div class="data-layout">
@@ -24,6 +70,7 @@
 		{#if colorCount > 1}
 			<div class="data-options form">
 				<ContrastTypeFilter />
+				<ContrastFilter />
 				<LuminosityFilter />
 			</div>
 		{/if}
@@ -31,40 +78,78 @@
 
 	<div>
 		{#if $colorEntries.length}
-			<table>
-				<caption>Test</caption>
-
-				<thead>
-					<tr>
-						<td />
+			<div role="table" aria-label="Color combinations">
+				<div role="rowgroup">
+					<div role="row">
+						<div />
 						{#each $colorEntries as { id, name, rgbColor } (id)}
-							<th scope="col">
-								<span
-									title="Foreground: {name}"
-									style="color: {hexColor(rgbColor)}"
+							<div role="columnheader">
+								<button
+									class="button"
+									title="Select all colors with foreground {name}"
+									on:click={() => selectForegrounds(id)}
 								>
-									Tt
-								</span>
-							</th>
+									<span
+										title="Foreground: {name}"
+										style="color: {hexColor(rgbColor)}"
+									>
+										Tt
+									</span>
+								</button>
+							</div>
 						{/each}
-					</tr>
-				</thead>
+					</div>
+				</div>
 
-				<tbody>
-					{#each $colorEntries as { id: bgId, name, rgbColor }, bgIndex (bgId)}
-						<tr style="--cbg: {hexColor(rgbColor)};">
-							<th scope="row">
-								<Sample
-									color={hexColor(rgbColor)}
-									title="background: {name}"
-								/>
-							</th>
+				<div role="rowgroup">
+					{#each $colorEntries as { id: bgId, name, color, rgbColor }, bgIndex (bgId)}
+						<div
+							role="row"
+							style="--cbg: {hexColor(
+								rgbColor
+							)}; --cbg-soft: {cssColor({
+								...rgbColor,
+								alpha: 0.5
+							})}; --cbg-light: {cssColor({
+								...color,
+								l: 95,
+								alpha: 0.5
+							})}; --cbg-dark: {cssColor({
+								...color,
+								l: 5,
+								alpha: 0.5
+							})};"
+						>
+							<div role="rowheader">
+								<button
+									class="button button--icon"
+									title="Select all colors with background {name}"
+									on:click={() =>
+										selectBackgrounds(bgId)}
+								>
+									<Sample
+										color={hexColor(rgbColor)}
+										title="background: {name}"
+									/>
+								</button>
+							</div>
+
 							{#each $colorEntries as { id: fgId }, fgIndex (fgId)}
-								<td
-									class="combination"
+								{@const level = levelFn(
+									rgbColor,
+									$colorEntries[fgIndex].rgbColor
+								)}
+								{@const grade = gradeFn(level)}
+								{@const roundedLevel = round(level, 1)}
+
+								<div
+									role="cell"
 									style="--cfg: {hexColor(
 										$colorEntries[fgIndex].rgbColor
-									)};"
+									)}; --cfg-soft: {cssColor({
+										...$colorEntries[fgIndex].rgbColor,
+										alpha: 0.1
+									})};"
 								>
 									{#if fgIndex !== bgIndex}
 										<input
@@ -92,18 +177,31 @@
 												}
 											}}
 										/>
+
 										<label
+											class="combination"
+											class:invalid={level <
+												$minLevel}
 											for="combination-{bgId}-{fgId}"
+											title="{$colorEntries[fgIndex]
+												.name} on {name}"
 										>
-											Tt
+											<span class="background button">
+												<span class="foreground">
+													<span class="grade">
+														{grade}
+													</span>
+													({roundedLevel})
+												</span>
+											</span>
 										</label>
 									{/if}
-								</td>
+								</div>
 							{/each}
-						</tr>
+						</div>
 					{/each}
-				</tbody>
-			</table>
+				</div>
+			</div>
 		{:else if colorCount > 1}
 			<p>
 				There is no color combination matching the selected
@@ -163,39 +261,137 @@
 		contain: layout;
 	}
 
-	table {
+	[role='table'] {
 		width: auto;
 		caption-side: bottom;
 	}
 
-	.combination {
-		padding: 0;
-		font-weight: bold;
+	[role='table'] :global(.sample) {
+		width: 1.5rem;
+		height: 1.5rem;
 	}
 
-	.combination label {
-		display: inline-block;
-		padding: 0.5rem;
-		background: var(--cbg);
-		color: var(--cfg);
-		font-weight: bolder;
-		font-family: var(--font-sans);
-		opacity: 0.1;
-		width: 5ch;
+	[role='row'] {
+		display: grid;
+		grid-auto-columns: 1fr;
+		grid-template-rows: 1fr;
+		align-items: stretch;
+		justify-content: stretch;
+	}
+
+	[role='row'] > * {
+		grid-row: 1;
+	}
+
+	[role='cell'] {
+		padding: 0;
 	}
 
 	input {
 		display: none;
 	}
 
-	.combination:hover,
-	input:checked ~ label {
-		opacity: 1;
-		outline: 1px solid var(--fg);
+	.combination {
+		display: block;
+		box-sizing: border-box;
+		padding: 3px 1.5px;
+		height: 100%;
 	}
 
-	.list :global(.sample) {
-		width: 1.5rem;
-		height: 1.5rem;
+	.background {
+		display: inline-block;
+		box-sizing: border-box;
+		border-radius: var(--radius);
+		padding: 0.5rem;
+		width: 100%;
+		height: 100%;
+		background: repeating-linear-gradient(
+			120deg,
+			transparent 0 calc(0.5ch - 1px),
+			var(--cbg) 0.5ch,
+			transparent calc(0.5ch + 1px)
+		);
+		/*
+		background-color: var(--cbg-light);
+		background-color: transparent;
+		background-image: repeating-linear-gradient(
+				45deg,
+				var(--cbg-dark) 25%,
+				transparent 25%,
+				transparent 75%,
+				var(--cbg-dark) 75%,
+				var(--cbg-dark)
+			),
+			repeating-linear-gradient(
+				45deg,
+				var(--cbg-dark) 25%,
+				var(--cbg-light) 25%,
+				var(--cbg-light) 75%,
+				var(--cbg-dark) 75%,
+				var(--cbg-dark)
+			);
+		background-position: 0 0, 0.1875rem 0.1875rem;
+		background-size: 0.375rem 0.375rem;
+		*/
+
+		//background-attachment: fixed;
+	}
+
+	.foreground {
+		font-family: var(--font-sans);
+		background: repeating-linear-gradient(
+			120deg,
+			var(--cfg-soft) 0 calc(0.25ch - 1px),
+			var(--cfg) 0.25ch,
+			var(--cfg-soft) calc(0.25ch + 1px) calc(0.5ch - 1px),
+			var(--cfg) 0.5ch,
+			var(--cfg-soft) calc(0.5ch + 1px)
+		);
+		background-clip: text;
+		-webkit-background-clip: text;
+		color: var(--cfg-soft);
+		background-attachment: fixed;
+		background-position: top left;
+	}
+
+	.grade {
+		font-weight: bold;
+	}
+
+	.combination:hover .background,
+	.combination:focus .background {
+		background: var(--cbg);
+	}
+
+	.combination:hover .foreground,
+	.combination:focus .foreground {
+		color: var(--cfg-soft);
+	}
+
+	input ~ .invalid {
+		opacity: 0.2;
+	}
+
+	input:checked ~ .combination,
+	.combination:hover,
+	.combination:focus {
+		opacity: 1;
+	}
+
+	input:checked ~ .combination .background {
+		box-shadow: 0 0 0 2px var(--fg), inset 0 0 0 1px var(--fg);
+		background: var(--cbg) !important;
+	}
+	/*
+	input:checked ~ .invalid .background {
+		outline-color: red;
+	}
+	*/
+	input:checked ~ .combination .foreground {
+		color: var(--cfg) !important;
+	}
+
+	input:checked ~ .invalid .foreground {
+		text-decoration: solid line-through currentColor 0.25ex;
 	}
 </style>
