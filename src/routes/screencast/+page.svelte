5263a06480737e2d851e479cebcfca0f62770c73
Félix Girault <felix.girault@gmail.com>
2022-11-16 14:00:12 +0100

    Extracted components out of routes

--- src/routes/screencast/+page.svelte
+++ src/routes/screencast/+page.svelte
@@ -1,38 +1,5 @@
 <script lang="ts">
-	// This page is only intended to be used to make screencasts
-	// of the app. The demo is done within a 800x600 screen,
-	// starting with #dbdb00, adding a contrasting color,
-	// a complementary color, then changing HSL hue of the
-	// root color to ~180, and finally to ~360.
-	import Index from '../+page.svelte';
+	import ScreencastApp from '$lib/components/ScreencastApp.svelte';
 </script>
 
-<svelte:head>
-	<meta name="robots" content="noindex" />
-</svelte:head>
-
-<Index />
-
-<style>
-	:global(.header),
-	:global(section footer),
-	:global(.combinations .data-options),
-	:global(.list > p),
-	:global(.hint),
-	:global(.preview-button) {
-		display: none !important;
-	}
-
-	:global(main) {
-		display: grid;
-		grid-template-columns: 40ch 1fr;
-	}
-
-	:global(section) {
-		padding-block: calc(var(--vgap) * 0.5) !important;
-	}
-
-	:global(.combinations .data-layout) {
-		display: block;
-	}
-</style>
+<ScreencastApp />
