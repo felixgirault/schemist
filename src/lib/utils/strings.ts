4e0f3b5bfe07fcfc69d8aa899d12eba1b7e829a3
Félix Girault <felix.girault@gmail.com>
2024-04-20 19:16:42 +0200

    Preventing illegal chars in CSS and Sass variables

--- src/lib/utils/strings.ts
+++ src/lib/utils/strings.ts
@@ -1,3 +1,8 @@
+// Sass is more liberal than CSS in what chars are legal in
+// a variable name, but we're taking the stricter approach.
+const stripForbiddenChars = (text: string) =>
+	text.replaceAll(/[^a-zA-Z0-9_-]/g, '');
+
 export const sentenceCase = (sentence: string) =>
 	sentence.charAt(0).toUpperCase() +
 	sentence.slice(1).toLowerCase();
@@ -5,12 +10,14 @@ export const sentenceCase = (sentence: string) =>
 export const dashCase = (sentence: string) =>
 	sentence
 		.split(' ')
+		.map(stripForbiddenChars)
 		.map((word) => word.toLowerCase())
 		.join('-');
 
 export const camelCase = (sentence: string) =>
 	sentence
 		.split(' ')
+		.map(stripForbiddenChars)
 		.map((word, index) =>
 			index === 0 ? word.toLowerCase() : sentenceCase(word)
 		)
